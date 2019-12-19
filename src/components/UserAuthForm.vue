456aaabcd91d5ec0efb52523ea165e06b97bee34
Dmitry Bochkov <dmitry.bochkov@gmail.com>
2019-12-19 18:13:24 +0200

    part 8 - reusable UserAuthForm omponent (signin + signup)

---
+++ src/components/UserAuthForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-form v-model="valid">
+    <v-text-field
+      v-model="userInfo.email"
+      label="Email"
+      type="email"
+      :rules="[required('Email'), emailFormat()]"
+    ></v-text-field>
+    <v-text-field
+      v-model="userInfo.password"
+      label="Password"
+      :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+      :type="showPassword ? 'text' : 'password'"
+      @click:append="showPassword = !showPassword"
+      :rules="[required('Password'), minLength('Password', 5)]"
+      :counter="true"
+    ></v-text-field>
+
+    <v-btn @click="submitForm(userInfo)" :disabled="!valid">{{ buttonText }}</v-btn>
+  </v-form>
+</template>
+
+<script>
+  import { required, minLength, emailFormat } from '@/utils/validations'
+
+  export default {
+    name: 'user-auth-form',
+    props: ['submitForm', 'buttonText'],
+    data() {
+      return {
+        valid: false,
+        userInfo: {
+          email: '',
+          password: ''
+        },
+        showPassword: false,
+        required,
+        minLength,
+        emailFormat
+      }
+    },
+  }
+</script>
+
+<style scoped>
+</style>
--- src/views/UserLogin.vue
+++ src/views/UserLogin.vue
@@ -2,51 +2,23 @@
   <v-container>
     <v-row justify="center">
       <v-col cols="12" sm="10" md="6" lg="4">
-        <v-form v-model="valid">
-          <v-text-field
-            v-model="loginInfo.email"
-            label="Email"
-            type="email"
-            :rules="[required('Email'), emailFormat()]"
-          ></v-text-field>
-          <v-text-field
-            v-model="loginInfo.password"
-            label="Password"
-            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-            :type="showPassword ? 'text' : 'password'"
-            @click:append="showPassword = !showPassword"
-            :rules="[required('Password'), minLength('Password', 5)]"
-            :counter="true"
-          ></v-text-field>
-
-          <v-btn @click="loginUser" :disabled="!valid">Login</v-btn>
-        </v-form>
+        <UserAuthForm :submitForm="loginUser" buttonText="Login" />
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-  import { required, minLength, emailFormat } from '@/utils/validations'
-  
+  import UserAuthForm from '@/components/UserAuthForm'
+
   export default {
     name: 'user-login',
-    data() {
-      return {
-        valid: false,
-        loginInfo: {
-          email: '',
-          password: ''
-        },
-        showPassword: false,
-        required,
-        minLength,
-        emailFormat
-      }
+    components: {
+      UserAuthForm
     },
     methods: {
-      async loginUser() {
-        const user = await this.$store.dispatch('loginUser', this.loginInfo)
+      async loginUser(loginInfo) {
+        const user = await this.$store.dispatch('loginUser', loginInfo)
 
         if (user.error) {
           alert(user.error)
